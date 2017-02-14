d6eb970bb0f9c1285157ae63524e6f8549ca61be
milesmao233 <milesmao233@gmail.com>
2017-02-14 11:55:26 +0800

    cannot add item with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,27 +1,33 @@
 class CartItemsController < ApplicationController
     before_action :authenticate_user!
 
-  def destroy
-    @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @product = @cart_item.product
-    @cart_item.destroy
+    def destroy
+        @cart = current_cart
+        @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+        @product = @cart_item.product
+        @cart_item.destroy
 
-    flash[:warning] = "成功将 #{@product.title} 从购物车删除!"
-    redirect_to :back
-  end
+        flash[:warning] = "成功将 #{@product.title} 从购物车删除!"
+        redirect_to :back
+    end
 
-  def update
-    @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+    def update
+        @cart = current_cart
+        @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+        if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
 
-    redirect_to carts_path
-  end
+            @cart_item.update(cart_item_params)
+            flash[:notice] = '成功变更数量'
+        else
+            flash[:warning] = '数量不足以加入购物车'
+       end
 
-  private
+        redirect_to carts_path
+    end
 
-  def cart_item_params
-    params.require(:cart_item).permit(:quantity)
-  end
+    private
+
+    def cart_item_params
+        params.require(:cart_item).permit(:quantity)
+    end
 end
