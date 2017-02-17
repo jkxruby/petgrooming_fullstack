0741722cde05dda906a40d97dbffa655bff7da36
milesmao233 <milesmao233@milesmao233deMacBook-Pro.local>
2017-02-17 16:46:34 +0800

    modify some defaults

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: [:destroy]
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
-  # POST /comments
-  # POST /comments.json
   def create
     @service = Service.find(params[:service_id])
     @comment = @service.comments.new(comment_params)
@@ -39,22 +18,8 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
+
   def destroy
     @service = Service.find(params[:service_id])
     @comment = Comment.find(params[:id])
